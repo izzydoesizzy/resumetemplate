01bba3589c7a599314d790400e8ca8dec019a681
Iskender Piyale-Sheard <ipiyale@gmail.com>
2016-06-29 17:55:30 +0100

    Finished

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,24 @@
-// Empty JS for your own code to be here
+// ResumeTemplate v1.0 ClearCareer
+// url: clearcareer.ca
+// by: Iskender Piyale-Sheard
+// website: izzydoesizzy.com
+// Created 06-27-2016
+ 
+
+// Smooth Scroll Function
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+
